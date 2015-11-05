1e3e0fc367249284537e3cb9a74094ff06246995
Eudald <eudald.sabate@est.fib.upc.edu>
2015-11-05 11:30:41 +0100

    Joc Sudoku

--- Sudoku/src/capaDomini/Joc.java
+++ Sudoku/src/capaDomini/Joc.java
@@ -4,29 +4,23 @@ package capaDomini;
 import capaDomini.Tauler;
 
 public abstract class Joc {
-	private int m, n;
 	private tipoDificultad dificultad;
 	private String id;
 	private Tauler tauler;
 	
 	// no es necessaria ja que no serveix
 	public Joc() {
-		m = n = 0;
 		dificultad = null;
 		id = null;
 	}
 	
 	public Joc(int m, int n, tipoDificultad dif, String idJoc) {
-		this.m = m;
-		this.n = n;
 		dificultad = dif;
 		id = idJoc;
 		tauler = new Tauler(m,n);
 	}
 	
-	public Joc(int m, int n, tipoDificultad dif, String idJoc, Tauler t) {
-		this.m = m;
-		this.n = n;
+	public Joc(tipoDificultad dif, String idJoc, Tauler t) {
 		dificultad = dif;
 		id = idJoc;
 		tauler = t;
@@ -34,10 +28,6 @@ public abstract class Joc {
 	
 	public String getId() {
 		return id;
-	}
-	
-	public int getN() {
-		return n;
 	}
 	
 	public tipoDificultad getDificultad() {
--- Sudoku/src/capaDomini/JocSudoku.java
+++ Sudoku/src/capaDomini/JocSudoku.java
@@ -1,20 +1,13 @@
 package capaDomini;
 
-import capaDomini.Tauler;
-
 public class JocSudoku extends Joc {
-
-	//potser no es necessaria ja que inicialitza el joc buit, i no ens pot interessar que aixo passi
-	public JocSudoku() {
-		super();
-	}
 	
 	public JocSudoku(int n, tipoDificultad dif, String idJocSudoku) {
 		super(n, n, dif, idJocSudoku);
 	}
 	
-	public JocSudoku(int n, tipoDificultad dif, String idJocSudoku, Tauler t) {
-		super(n, n, dif, idJocSudoku, t);
+	public JocSudoku(tipoDificultad dif, String idJocSudoku, TaulerSudoku t) {
+		super(dif, idJocSudoku, t);
 	}
 
 	public void pista() {
