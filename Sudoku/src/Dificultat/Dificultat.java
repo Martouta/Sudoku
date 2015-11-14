2060b804c3755ee53368aa473c46c9afe4f31187
Mrt92 <marta.noya@est.fib.upc.edu>
2015-11-14 17:17:05 +0100

    clases compartidas companeros

--- Sudoku/src/Dificultat/Dificultat.java
+++ Sudoku/src/Dificultat/Dificultat.java
@@ -1,7 +1,7 @@
 package Dificultat;
 
 abstract public class Dificultat {
-	private static final String[] d = {"4x4", "9x9", "16x16"};
+	private static final String[] d = {"3x3", "4x4", "5x5", "6x6", "7x7", "8x8", "9x9"};
 	
 	public static boolean esValida(String s) {
 		for (int i=0; i<d.length; ++i) {
@@ -12,5 +12,16 @@ abstract public class Dificultat {
 	
 	public static String[] getAll() {
 		return d;
+	}
+	
+	/* La dificultat d és valida */
+	public static int toInt(String s) {
+		int i = 0;
+		boolean trobat = false;
+		while (!trobat) {
+			if (d[i].equals(s)) trobat = true;
+			else ++i;
+		}
+		return Integer.parseInt(d[i].substring(0,1));
 	}
 }
---
+++ Sudoku/src/Ranking/RankingFactory.java
@@ -0,0 +1,25 @@
+package Ranking;
+
+import persistencia.*;
+import capaDomini.*;
+
+public class RankingFactory {
+	
+	public RankingFactory() {
+		CtrlPersistencia.setSeparator(" ");
+		System.out.println("---");
+		CtrlUser CU = new CtrlUser();
+	}
+	
+	public RankingGeneral generarRankingGeneral() {
+		return new RankingGeneral();
+	}
+	
+	public RankingPerTipus generarRankingPerTipus (String d, int nEntrades) {
+		return new RankingPerTipus(d, nEntrades);
+	}
+	
+	public RankingPersonal generarRankingPersonal(String username) {
+		return new RankingPersonal(username);
+	}
+}
