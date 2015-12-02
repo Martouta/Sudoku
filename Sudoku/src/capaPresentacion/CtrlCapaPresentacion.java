b0ea37811f0c83eebe128d7d7c0b3059b668d1b5
Mrt92 <marta.noya@est.fib.upc.edu>
2015-12-02 16:50:02 +0100

    mas cosas

--- Sudoku/src/capaPresentacion/CtrlCapaPresentacion.java
+++ Sudoku/src/capaPresentacion/CtrlCapaPresentacion.java
@@ -9,6 +9,10 @@ import excepciones.excUsuario.*;
 public class CtrlCapaPresentacion {
 	private static JFrame frameMenuPrincipal;
     private static JFrame frameIniciarSesion;
+    private static JFrame frameRegistrarse;
+    private static JFrame frameMenuOpciones;
+    private static JFrame frameMenuSudoku;
+    
     
 	private static Boolean cambiosParaBD;
 	private static String nombreUsuario;
@@ -19,8 +23,9 @@ public class CtrlCapaPresentacion {
 		initComponents();
 		initListeners();
 		frameMenuPrincipal.setVisible(true);
+		cargarTodo();
 	}
-	
+
 	private void initComponents() {
 		//INIT MENU PRINCIPAL:
 		JFrame.setDefaultLookAndFeelDecorated(true);
@@ -28,69 +33,97 @@ public class CtrlCapaPresentacion {
         frameMenuPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frameMenuPrincipal.pack();
         
-        //INIT INICIAR SESIÓN:
+        //INIT INICIAR SESION:
         JFrame.setDefaultLookAndFeelDecorated(true);
         frameIniciarSesion = new JFrameIniciarSesion();
         frameIniciarSesion.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frameIniciarSesion.pack();
+        
+        //INIT REGISTRARSE:
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        frameRegistrarse = new JFrameRegistrarse();
+        frameRegistrarse.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frameRegistrarse.pack();
+        
+        //INIT MENU OPCIONES:
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        frameMenuOpciones = new JFrameMenuOpciones();
+        frameMenuOpciones.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frameMenuOpciones.pack();
+        
+        //INIT MENU SUDOKU:
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        frameMenuSudoku = new JFrameRegistrarse();
+        frameMenuSudoku.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frameMenuSudoku.pack();
 	}
 	
 	private void initListeners() {
+		initListenersMenuPrincipal();
+		initListenersIniciarSesion();
+	}
+	
+	private void initListenersMenuPrincipal() {
 		//LISTENERS DE MENU PRINCIPAL:
 		((JFrameMenuPrincipal) frameMenuPrincipal).getButSalir().addActionListener(new ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                pressSalir();
-            }
-        });
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				pressSalir();
+		    }
+		});
 		((JFrameMenuPrincipal) frameMenuPrincipal).getButIniciarSesion().addActionListener(new ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-            	frameMenuPrincipal.setVisible(false);
-            	frameIniciarSesion.setVisible(true);
-            }
-        });
+				public void actionPerformed(java.awt.event.ActionEvent evt) {
+					frameMenuPrincipal.setVisible(false);
+					frameIniciarSesion.setVisible(true);
+				}
+		});
 		((JFrameMenuPrincipal) frameMenuPrincipal).getButRegistrarse().addActionListener(new ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                //FALTA: ocultar esta ventana y abrir la de registrarse
-            }
-        });
-		
+				public void actionPerformed(java.awt.event.ActionEvent evt) {
+					frameMenuPrincipal.setVisible(false);
+					frameRegistrarse.setVisible(true);
+				}
+		});
+	}
+	
+	private void initListenersIniciarSesion() {
 		//LISTENERS DE INICIAR SESIÓN:
 		((JFrameIniciarSesion) frameIniciarSesion).getButSalir().addActionListener(new ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                pressSalir();
-            }
-        });
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				pressSalir();
+			}
+		});
 		((JFrameIniciarSesion) frameIniciarSesion).getButIniciarSesion().addActionListener(new ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-            	CtrlCasoUsoIniciarSesion ctrlCUIniciarSesion = new CtrlCasoUsoIniciarSesion();
-            	try {
-            		nombreUsuario = ((JFrameIniciarSesion) frameIniciarSesion).getNombreUsuario();
-            		if (nombreUsuario.isEmpty()) throw (new ExcepcionCamposVacios());
-            		String contrasena = ((JFrameIniciarSesion) frameIniciarSesion).getContrasena();
-            		ctrlCUIniciarSesion.iniciarSesion(nombreUsuario, contrasena);
-            		//Si llega hasta aqui es que ha funcionado sin ninguna excepcion
-            		frameIniciarSesion.setVisible(false);
-            		//FALTA abrir venatana de "MenuOpciones"
-            		System.out.println("[Mensaje temporal] Sesion iniciada con el usuario " + nombreUsuario + " con contrasena " + contrasena);
-            		
-            	} catch (ExcepcionCamposVacios e) {
-            		nombreUsuario = "";
-            		((JFrameIniciarSesion) frameIniciarSesion).setMensajeError(e.getMessage());
-            	} catch (ExcepcionUsuarioNoExiste e) {
-            		nombreUsuario = "";
-            		((JFrameIniciarSesion) frameIniciarSesion).setMensajeError(e.getMessage());
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				CtrlCasoUsoIniciarSesion ctrlCUIniciarSesion = new CtrlCasoUsoIniciarSesion();
+				try {
+					nombreUsuario = ((JFrameIniciarSesion) frameIniciarSesion).getNombreUsuario();
+					if (nombreUsuario.isEmpty()) throw (new ExcepcionCamposVacios());
+					String contrasena = ((JFrameIniciarSesion) frameIniciarSesion).getContrasena();
+					ctrlCUIniciarSesion.iniciarSesion(nombreUsuario, contrasena);
+					//Si llega hasta aqui es que ha funcionado sin ninguna excepcion
+					frameIniciarSesion.setVisible(false);
+					//FALTA abrir venatana de "MenuOpciones"
+					System.out.println("[Mensaje temporal] Sesion iniciada con el usuario " + nombreUsuario + " con contrasena " + contrasena);		
+				} catch (ExcepcionCamposVacios e) {
+					nombreUsuario = "";
+					((JFrameIniciarSesion) frameIniciarSesion).setMensajeError(e.getMessage());
+				} catch (ExcepcionUsuarioNoExiste e) {
+					nombreUsuario = "";
+					((JFrameIniciarSesion) frameIniciarSesion).setMensajeError(e.getMessage());
 				} catch (ExcepcionContrasenaIncorrecta e) {
 					nombreUsuario = "";
 					((JFrameIniciarSesion) frameIniciarSesion).setMensajeError(e.getMessage());
 				}
-            }
-        });
+			}
+		});
 	}
-
     
     private void pressSalir() {
 		//FALTA: guardar en txt o no, dependera del valor cambiosParaBD
 		System.out.println("Salir pulsado");
+	}
+    
+    private void cargarTodo() {
+		//POR HACER
 	}
 	
 }
