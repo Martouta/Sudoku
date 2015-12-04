29ca125a390e2dba8e29ff8ceadc74984838d471
Eudald <eudald.sabate@est.fib.upc.edu>
2015-12-04 23:31:26 +0100

    Canvi al path de CtrlUser

--- Sudoku/src/persistencia/CtrlUser.java
+++ Sudoku/src/persistencia/CtrlUser.java
@@ -28,7 +28,7 @@ public class CtrlUser
 {
 	private static boolean dirty;             // true si s'ha modificat la llista d'usuari
 	protected static ArrayList<User> usuaris; // ordenats per nom
-    private static String path = "src/domini/data/users.txt";
+    private static String path = "src/domini/data/users.txt"; //Canvi del path 
 	
 	// Carrega els usuaris de la BD
 	// si hi ha hagut error al carregar els usuaris llença una excepcio
